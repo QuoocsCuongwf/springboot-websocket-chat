6ab071a569b10c39c95787ad3770942eddb686ac
Le Quoc Cuong <lequoccuong2204@gmail.com>
2026-09-22 10:12:05 +0700

    first commit

---
+++ nextchat/src/test/java/com/quoocscuongwf/nextchat/NextchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quoocscuongwf.nextchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
